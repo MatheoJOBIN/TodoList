fe62394de8c771d1c205207957d2cb3c14a45cf8
MatheoJOBIN <80678160+MatheoJOBIN@users.noreply.github.com>
2022-02-18 14:26:39 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB4jrS4rqI2Yq3U_JOYfPz3pwZBrVfAOUM",
-    authDomain: "test-1-38325.firebaseapp.com",
-    projectId: "test-1-38325",
-    storageBucket: "test-1-38325.appspot.com",
-    messagingSenderId: "649011293718",
-    appId: "1:649011293718:web:603fd3346956eeceb94f37",
-    measurementId: "G-ZZYN86BNWW"
+    Config
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
